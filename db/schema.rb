c5cc49dfebe0383e5e11a91df31319844370cb9b
Sasa Radojicic <kalabaster@gmail.com>
2016-06-05 16:29:40 +0000

    Added upload image and styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604180647) do
+ActiveRecord::Schema.define(version: 20160605160733) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -38,6 +38,10 @@ ActiveRecord::Schema.define(version: 20160604180647) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: true do |t|
